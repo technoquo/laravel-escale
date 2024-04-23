88f7294f83d22755af127e47cba00980698c13b9
Leonel Lopez <technoquo@gmail.com>
2024-04-23 22:03:51 +0200

    ajustes de nuevo

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,13 +10,6 @@ class PostController extends Controller
     public function index($slug)
     {
         $post = Post::where('slug', $slug)->where('status', 1)->first();
-        
-        return view('posts.post', [
-            'post' => $post,
-            'ogDescription' => $post->title,
-            'ogUrl' => config('app.url').'/'.$post->slug, // Assuming you have a route named 'posts.show' for viewing individual posts
-            'ogImage' => config('app.url').'/storage/'. $post->image, // Assuming the image path is stored in the 'image' column of the 'posts' table
-            //'keywords' => $post->keywords,
-        ]);
+        return view('posts.post', compact('post'));
     }
 }
